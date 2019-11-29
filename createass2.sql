21e77c87757036daaa4f9c6dfc438cc52d9eaebc
Bao Phuc Nguyen <phuc.nguyenbao@hcmut.edu.vn>
2019-11-29 14:26:51 +0700

    Update primary key scripts

--- createass2.sql
+++ createass2.sql
@@ -1,26 +1,30 @@
 CREATE TABLE Users(
 	userid CHAR(10) NOT NULL,
-	name VARCHAR(15) NOT NULL,
+	uname VARCHAR(15) NOT NULL,
 	sex CHAR(1),
 	bdate date,
 	address VARCHAR(30),
 	password CHAR(32) NOT NULL,
 	username VARCHAR(15) NOT NULL,
+	PRIMARY KEY (userid),
 );
 CREATE TABLE Phone(
 	userid CHAR(10) NOT NULL,
 	phone VARCHAR(11),
+	PRIMARY KEY (userid),
 );
 CREATE TABLE Student(
-	userid CHAR(10) NOT NULL,
+	studentid CHAR(10) NOT NULL,
 	loaiHV CHAR(1),
 	trialID CHAR(10),
 	expireddate date,
+	PRIMARY KEY (userid),
 );
 
 CREATE TABLE Teacher(
-	userid CHAR(10) NOT NULL,
+	teacherid CHAR(10) NOT NULL,
 	balance DECIMAL (10,2),
+	PRIMARY KEY (userid),
 );
 
 
@@ -28,12 +32,14 @@ CREATE TABLE Topic(
 	tid CHAR(5) NOT NULL,
 	tname VARCHAR(30),
 	tdesc VARCHAR(200),
+	PRIMARY KEY (tid),
 );
 
 CREATE TABLE Curriculum(
 	curid CHAR(5) NOT NULL,
 	cuname VARCHAR(30),
 	cudesc VARCHAR(200),
+	PRIMARY KEY (curid),
 );
 
 CREATE TABLE Course(
@@ -42,40 +48,46 @@ CREATE TABLE Course(
 	cdesc VARCHAR(300),
 	cstatus CHAR(1),
 	price DECIMAL(10,2),
-
+	PRIMARY KEY (courseid),
 );
 
 CREATE TABLE Teach(
-	userid CHAR(10) NOT NULL,
+	teacherid CHAR(10) NOT NULL,
 	courseid CHAR(8) NOT NULL,
+	PRIMARY KEY (userid,courseid),
 );
 
 CREATE TABLE Curricourse(
-	cuserid CHAR(5) NOT NULL,
+	curid CHAR(5) NOT NULL,
 	courseid CHAR(8) NOT NULL,
+	PRIMARY KEY(curid,courseid),
 );
 
 CREATE TABLE Topiccourse(
 	tid CHAR(5) NOT NULL,
 	courseid CHAR(8) NOT NULL,
+	PRIMARY KEY (tid,courseid),
 );
 
 CREATE TABLE Enroll(
-	userid CHAR(10) NOT NULL,
+	studentid CHAR(10) NOT NULL,
 	courseid CHAR(8) NOT NULL,
 	epoint DECIMAL (2,2),
+	PRIMARY KEY(userid,courseid),
 );
 
 CREATE TABLE Assessment(
-	userid CHAR(10) NOT NULL,
+	studentid CHAR(10) NOT NULL,
 	courseid CHAR (8) NOT NULL,
 	apoint DECIMAL(2,2),
+	PRIMARY KEY(userid,courseid),
 );
 
 CREATE TABLE Lesson(
 	courseid CHAR(8) NOT NULL,
 	lid CHAR(2) NOT NULL,
 	lname NVARCHAR,
+	PRIMARY KEY(courseid,lid),
 )
 
 CREATE TABLE Document(
@@ -83,6 +95,7 @@ CREATE TABLE Document(
 	lid CHAR(2) NOT NULL,
 	docid CHAR(2) NOT NULL,
 	link VARCHAR(100),
+	PRIMARY KEY (courseid,lid,docid),
 );
 
 CREATE TABLE Dicussion(
@@ -91,40 +104,57 @@ CREATE TABLE Dicussion(
 	courseid CHAR(8) NOT NULL,
 	startdate DATE,
 	content NVARCHAR,
+	PRIMARY KEY (dicid),
 );
 
 CREATE TABLE Comment(
-	courseid CHAR(8) NOT NULL,
+	dicid CHAR(8) NOT NULL,
 	commentid CHAR(8) NOT NULL,
 	content NVARCHAR,
 	ctime datetime,
+	PRIMARY KEY(dicid,commentid),
 );
 
 CREATE TABLE Examination(
 	eid CHAR(5) NOT NULL,
 	courseid CHAR(8) NOT NULL,
+	PRIMARY KEY (eid),
 );
 
 CREATE TABLE Exam(
 	examid CHAR(5) NOT NULL,
 	userid CHAR(10) NOT NULL,
 	takentime datetime,
+	PRIMARY KEY (examid,userid),
 );
 
 CREATE TABLE Question(
 	qid CHAR(8) NOT NULL,
 	content NVARCHAR,
 	point DECIMAL(2,2),
+	PRIMARY KEY (qid),
 );
 
 CREATE TABLE ExaminationQuestion(
 	eid CHAR(5) NOT NULL,
 	qid CHAR(8) NOT NULL,
+	PRIMARY KEY (eid,qid),
 );
+
 
 CREATE TABLE Options(
 	qid CHAR(8) NOT NULL,
 	onumber CHAR(3),
 	content NVARCHAR,
 	result CHAR(1),
+	PRIMARY KEY (qid,onumber),
+);
+
+CREATE TABLE ExamOption(
+	eid CHAR(5) NOT NULL,
+	studentid CHAR(10) NOT NULL,
+	onumber CHAR(3),
+	qid CHAR(8),
+	examresult DECIMAL(2,2),
+	PRIMARY KEY (eid,studentid,onumber,qid),
 );
