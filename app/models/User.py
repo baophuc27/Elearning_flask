7d7f561e994f6b51a581a3825e044a3777750cc4
lungocthientruc <lungocthientruc@gmail.com>
2019-12-06 14:13:39 +0700

    add reg

--- app/models/User.py
+++ app/models/User.py
@@ -5,12 +5,52 @@ class Users():
 class query():
     @staticmethod
     def getNameandPass(db,name,password):
-        sql="select * from users where uname=\'"+name+ "\' and password=\'"+password +"\'"
+        sql="select dbo.checklogin (\'"+name +"\', \'" + password +"\')" 
         result = db.engine.execute(sql)
-        if result is not None:
+        result = result.fetchone()
+        if result[0] == True:
             return True
         else:
             return False
+
+    @staticmethod
+    def checkifnameregisted(db,name):
+        sql="select dbo.checknameregisted (\'"+name + "\')" 
+        result = db.engine.execute(sql)
+        result = result.fetchone()
+        if result[0] == True:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def createnewuser(db,uname,sex,bdate,address,password,username,role):
+        if address=='':
+            sql="exec dbo.insertinuser \'"+uname+"\',\'"+sex+"\',\'"+bdate+"\',NULL,\'"+password+"\',N\'"+username+"\'"
+        else:
+            sql="exec dbo.insertinuser \'"+uname+"\',\'"+sex+"\',\'"+bdate+"\',N\'"+address+"\',\'"+password+"\',N\'"+username+"\'"
+        connection=db.engine.connect()
+        trans=connection.begin()
+        connection.execute(sql)
+        trans.commit()
+        #select id of new user
+        sql="select userid from Users where (uname=\'"+uname+"\')"
+        connection=db.engine.connect()
+        trans=connection.begin()
+        result=connection.execute(sql)
+        userid=result.fetchone()
+        userid=userid[0]
+        #add into teacher or student
+        if role=='Student':
+            sql="exec dbo.insertinstudent "+str(userid)+",'T',NULL,NULL"
+        else:
+            sql="exec dbo.insertinteacher "+str(userid)+",NULL"
+        connection=db.engine.connect()
+        trans=connection.begin()
+        error=connection.execute(sql)
+        print("error {0}".format(error.fetchone()))
+        #trans.commit()
+       
 
 
 
